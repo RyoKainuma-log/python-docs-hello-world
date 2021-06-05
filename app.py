9e03835d73dc5d0d307341c4269a94303fb5e329
kainuma.logbuild@gmail.com <ryo.kainuma@unibot.co.jp>
2021-06-06 08:56:10 +0900

    add render_templates

--- app.py
+++ app.py
@@ -1,10 +1,20 @@
-from flask import Flask
+from flask import Flask, render_template
 import requests
 app = Flask(__name__)
 
 @app.route("/")
-def hello():
+def index():
+    
+    return render_template('index.html')
+
+
+@app.route("/request")
+def request():
     res = requests.get('https://knmhttotriggerfunction.azurewebsites.net/api/knmHttpTriggerFunction', data={'name': 'hello azure'})
     res_txt = res.text
 
     return res_txt
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
